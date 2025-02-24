30c8d7f0433f738c34752fe903bce1d9184ba6cb
Thoams Vigh <tvigh2@illinois.edu>
2025-02-23 18:31:07 -0600

    making a copy for 5.1/5.2

---
+++ Lab5.2/srcs/MUX.sv
@@ -0,0 +1,41 @@
+module MUX_4bit {
+    input   logic [15:0] in0,
+    input   logic [15:0] in1,
+    input   logic [15:0] in2,
+    input   logic [15:0] in3,
+    input   logic [1:0] select,
+
+    output  logic [15:0]] out
+};
+    always_comb
+    begin 
+        if (select == 2'b00) begin
+            out = in0;
+        end
+        else if (select == 2'b01) begin
+            out = in1;
+        end
+        else if (select == 2'b10) begin
+            out = in2;
+        end
+        else if (select == 2'b11) begin
+            out = in3;
+        end
+    end
+endmodule
+
+module MUX_2bit {
+    input logic [15:0] in0,
+    input logic [15:0] in1,
+    input logic select,
+    
+    output logic [15:0] out,
+};
+    always_comb begin 
+        if (select == 1'b0) begin
+            out = in0;
+        end else begin
+            out = in1;
+        end 
+    end   
+endmodule
